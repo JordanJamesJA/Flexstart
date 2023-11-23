9cf33f036449b497372cd65ffbef9d36307d43cc
JordanJamesJA <97801730+JordanJamesJA@users.noreply.github.com>
2023-11-23 01:09:33 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,16 @@
 //NAVBAR RESPONSIVE
-
+let navbar = document.getElementsByClassName("navbar")[0];
 let active = document.getElementById("activePage");
 let toggleButton = document.getElementById("icon");
 
 function toggleActiveId() {
-    active.classList.toggle("active");
+  active.classList.toggle("active");
+
+  if (active.classList.contains("active")) {
+    navbar.style.background = "white";
+  } else {
+    navbar.style.background = "";
+  }
 }
 
 toggleButton.addEventListener("click", toggleActiveId);
